d58fd09bf04d52290f1895a6d2ae31f4065ceff6
Geerocks <54655165+Geerocks@users.noreply.github.com>
2020-03-19 13:36:43 -0700

    R packages

--- test.R
+++ test.R
@@ -1,9 +1,15 @@
 install.packages("RMySQL")
 install.packages("RMariaDB")
+install.packages("DBI")
+install.packages("odbc")
+install.packages("RSQLite")
+library(odbc)
+library(dbConnect)
 library(DBI)
 con <- dbConnect(RMySQL::MySQL(),
-    dbname = "TwitterDB.tesla",
+    dbname = "TwitterDB",
     host = "localhost",
     port = 3306,
     user = "root",
     password = "gaurav")
+dbReadTable(con, "tesla")
